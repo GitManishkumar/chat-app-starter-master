38cd96deba48080c1689a0391dd348672c6a032b
Manish Kumar <manishkumar96715@gmail.com>
2023-06-30 15:16:40 +0530

    started building the sidebar components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Grid, Row, Col} from 'rsuite'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <Grid fluid classname="h-100">
+    <Row>
+      <Col xs={24} md={8}>
+        <Sidebar/>
+      </Col>
+    </Row>
+    </Grid>
   )
 }
 
